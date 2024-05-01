db9c7e1247f5772d3251bbe446a4cf424f2a5f5b
Marcus <marcus.melcher1@gmail.com>
2024-05-01 05:36:55 +0300

    Adjust namespaces and folder structures

---
+++ Blip/Avalonia/Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("https://github.com/blippyblips/Blip.Avalonia.Extensions", "Blip.Avalonia.Extensions")]
+[assembly: XmlnsDefinition("https://github.com/blippyblips/Blip.Avalonia.Extensions", "Blip.Avalonia.Extensions.AutoEditor")]
--- Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://github.com/blippyblips/ex", "Blip.Avalonia.Extensions")]
-[assembly: XmlnsDefinition("https://github.com/blippyblips/ex", "Blip.Avalonia.Extensions.AutoEditor")]
